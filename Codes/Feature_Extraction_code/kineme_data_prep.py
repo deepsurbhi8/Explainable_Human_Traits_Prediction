dc0864c80fd34cf781aaf7242f3191b963c3a3f6
Monika <79365852+MonikaGahalawat11@users.noreply.github.com>
2024-02-07 01:32:10 +1100

    Update kineme_data_prep.py comments

--- Codes/Feature_Extraction_code/kineme_data_prep.py
+++ Codes/Feature_Extraction_code/kineme_data_prep.py
@@ -1,4 +1,8 @@
 #Function to create kineme data matrix for Train Data
+# chunk_time - chunk duration considered for the analysis
+# input_file_kineme - kineme values csv file
+# label_val - label value for the file
+# num_chunk - number of chunks in the entire video file
 def data_preprocess(chunk_time, input_file_kineme, label_val,num_chunk): 
     data_list = []
     total_num_chunks = 0
@@ -8,7 +12,8 @@ def data_preprocess(chunk_time, input_file_kineme, label_val,num_chunk):
     #print("Creating the input from csv files")
     final_data_file = input_file_kineme.T
     #num_chunk = int(size_file/chunk_time)
-        
+
+    # dividing the entire data into the num of chunks
     for num in range(0, num_chunk):
         data_chunk = final_data_file[num*(chunk_time-1):((num+1)*(chunk_time-1))]
         data_chunk = data_chunk.to_numpy()
